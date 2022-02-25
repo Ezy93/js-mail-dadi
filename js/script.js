2611a1bf17ebb84030d8c10cabd23f3e0a6de540
Ezy93 <giordani001@gmail.com>
2022-02-25 17:17:09 +0100

    problema con confronto tra elementi array e let

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
-let userMail = document.getElementById("mail");
+
 let userSubmit = document.getElementById("submit");
 
 let mailList = ["harder@gmail.com" , "better@outlook.it" , "faster@yahoo.it", "stronger@leonardo.it"];
-
+console.log(mailList)
 
 userSubmit.addEventListener('click', function(){
 
+    let userMail = document.getElementById("mail").value;
+
+    for(let i = 0 ; i < 4 ; i++ ){
+        
+        if ( mailList[i] == userMail.value ){
+
+            document.getElementById("output").innerHTML = "complimenti sei nella mailing list";
+            
+        } else {
+
+            document.getElementById("output").innerHTML = "ci dispiace ma non risulti nella nostra mailing list";
+
+        }
     
-    console.log(userMail.value);
+        console.log(mailList[i])
+    }
+
 
 });
+
