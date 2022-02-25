86008f47fc1432d34e6950aa9abdd3c408f65f43
Ezy93 <giordani001@gmail.com>
2022-02-25 18:25:06 +0100

    AGGIUNTO PROMEMORIA PER DOPO PAUSA

--- js/script.js
+++ js/script.js
@@ -13,22 +13,28 @@ userSubmit.addEventListener('click', function(){
     let userMail = document.getElementById("mail").value;
 
     /* eseguo un ciclo prmettere all'operatore di confronto di passare tutti gli elementi dell'array  */
-    for(let i = 0 ; i <= 3 ; i++ ){
+    for(let i = 0 ; i <= 3 ; i++ ){ 
+
         /* eseguo un operatore di confronto per confrontare se l'input dell'utente corrisponde ad uno degli elementioi dell'array */
         if( mailList[i] === userMail ){
+
             /* se l'input corrisponde verra scritto sullo schermo il seguente messaggio */
-            document.getElementById("output").innerHTML = "complimenti sei nella mailing list";
+            document.getElementById("output").innerHTML = "complimenti sei autorizzato al gioco clandestino  nella nostra bisca";
+
+            //TODO: AGGIUNGERE IL GIOCO DEI DADI SOLO SE L'UTENTE HA ACCESSO 
+
             /* aggiungo la parola chiave break in quanto serve per uscire dal ciclo se viene trovata una corrispondenza tra l'input utente e un elemento dell'array,
             altrimenti l'operatore di confronto non funziona */
             break;
 
         } else {
+
             /* messaggio scritto sullo schermo nel caso in cui l'input utente noon corrisponda a nessuno degli elementi dell'array */
-            document.getElementById("output").innerHTML = "ci dispiace ma non risulti nella nostra mailing list";
+            document.getElementById("output").innerHTML = "non sei autorizzato al gioco clandestino  nella nostra bisca";
 
         }
     
-        /* console.log(typeof mailList[i]) */
+        
         
     }
 
