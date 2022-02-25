d5c4e6f141adf2ee35060d2b630dc806b20758bf
Ezy93 <giordani001@gmail.com>
2022-02-25 16:39:04 +0100

    risolto errore per l'eventlistener

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let userMail = document.getElementById("mail");
+let userSubmit = document.getElementById("submit");
+
+let mailList = ["harder@gmail.com" , "better@outlook.it" , "faster@yahoo.it", "stronger@leonardo.it"];
+
+
+userSubmit.addEventListener('click', function(){
+
+    
+    console.log(userMail.value);
+
+});
